e30d92e9d84d8ff92ca48ee6bc03429883cdc0aa
Jendobas <jendobas@gmail.com>
2025-06-26 19:09:54 +0300

    first view

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def company_details(request):
+    return Response({
+        'company_name': 'Ночной забег',
+        'slogan': 'Ноль - тоже результат',
+        'contacts': 'Город Москва, улица Пушкина, дом 3'
+    })
