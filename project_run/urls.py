8280c61503eb95cb3fa4e447badb395ddc126e00
Jendobas <jendobas@gmail.com>
2025-06-28 16:53:59 +0300

    create view GerUsers and serializer UserSerializers

--- project_run/urls.py
+++ project_run/urls.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details, RunViewSet
+from app_run.views import company_details, RunViewSet, GetUsers
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
+router.register('api/users', GetUsers)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
+    path('', include(router.urls)),
     path('', include(router.urls))
 ]
