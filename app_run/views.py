6db41547de18cca8bbc1544a0b70effc2c23e4ff
Jendobas <jendobas@gmail.com>
2025-06-29 18:56:18 +0300

    add search in GetUsers

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -26,6 +27,9 @@ class RunViewSet(viewsets.ModelViewSet):
 class GetUsers(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.filter(is_superuser=False)
     serializer_class = UserSerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ['first_name', 'last_name']
 
     def get_queryset(self):
         qs = self.queryset
