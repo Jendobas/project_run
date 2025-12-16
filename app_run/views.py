e4c8043d98e5c6e11d1192adf1215d281953db7f
Jendobas <jendobas@gmail.com>
2025-12-16 23:09:26 +0300

    bug fix 1.0

--- app_run/views.py
+++ app_run/views.py
@@ -38,10 +38,11 @@ def total_running_time_in_seconds(run_id):
         first_date=Min('date_time'),
         last_date=Max('date_time')
     )
-    try:
+
+    if not result['last_date'] or not result['first_date']:
+        duration = 0
+    else:
         duration = result['last_date'] - result['first_date']
-    except TypeError:
-        raise TypeError("Ожидалось число, а пришло None или неподходящий тип")
     total_seconds = int(duration.total_seconds())
     res = Run.objects.get(pk=run_id)
     res.run_time_seconds = total_seconds
